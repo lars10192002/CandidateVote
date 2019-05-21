37d0f3c14db52f5386f9c6c7aae21e7dc83f19e9
LucaKao <lars10192002@gmail.com>
2019-05-21 14:52:17 +0800

    [init] Create Cancidate

---
+++ app/controllers/candidates_controller.rb
@@ -0,0 +1,28 @@
+class CandidatesController < ActionController::Base
+    def index
+      @candidates = Candidate.all
+    end
+
+    def new
+      @candidate = Candidate.new
+    end
+
+    def create
+      @candidate = Candidate.new(candidate_params)
+  
+      if @candidate.save
+        flash[:notice] = '新增成功'
+        redirect_to "/"
+      else
+        flash[:notice] = '新增失敗'
+        render :new
+      end
+    end
+    
+      private
+    
+      def candidate_params
+        params.require(:candidate).permit(:name, :age, :party, :platform)
+      end
+
+end
